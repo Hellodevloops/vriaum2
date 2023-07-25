de61c530e6addd9bedb4429371094eff30671c16
Richa24vaidya <richa@devloops.in>
2023-07-25 21:05:54 +0530

    richa_new commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,25 +91,25 @@
                                             {{-- <h6 class="drop-heading">IT Development </h6>  --}}
 
                                             <a href="{{ route('iot') }}" class="dropdown-link px-0">
-                                                <span class="me-2">
+                                            <span class="me-2">
                                                     <i class=" flaticon-fingerprint"></i>
                                                 </span>
                                                 <div class="drop-title">Internet Of Things</div>
                                             </a>
                                             <a href="{{ route('webapp') }}" class="dropdown-link px-0">
-                                                <span class="me-2">
+                                            <span class="me-2">
                                                     <i class="flaticon-web-programming"></i>
                                                 </span>
                                                 <div class="drop-title">Web Development</div>
                                             </a>
                                             <a href="{{ route('mobileapp') }}" class="dropdown-link px-0">
-                                                <span class="me-2">
+                                            <span class="me-2">
                                                     <i class="flaticon-layer"></i>
                                                 </span>
                                                 <div class="drop-title">Mobile App Development</div>
                                             </a>
                                             <a href="{{ route('product') }}" class="dropdown-link px-0">
-                                                <span class="me-2">
+                                                 <span class="me-2">
                                                     <i class="flaticon-vector"></i>
                                                 </span>
                                                 <div class="drop-title">Product Development</div>
@@ -231,38 +231,88 @@
                                         <h6 class="drop-heading">IT Consulating</h6>
 
                                         <a href="{{ route('iot') }}" class="dropdown-link">
-                                            <span class="me-2">
-                                                <i class="flaticon-layer"></i>
-                                            </span>
-                                            <div class="drop-title">Software Devlopment</div>
+                                        <span class="me-2">
+                                                    <i class=" flaticon-fingerprint"></i>
+                                                </span>
+                                            <div class="drop-title">Internet Of Things</div>
                                         </a>
-                                        <a href="{{ route('iot') }}" class="dropdown-link">
-                                            <span class="me-2">
-                                                <i class="flaticon-phone-book"></i>
-                                            </span>
-                                            <div class="drop-title">IOT Devlopment</div>
+                                        <a href="{{ route('webapp') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <i class="flaticon-web-programming"></i>
+                                                </span>
+                                            <div class="drop-title">Web Development</div>
                                         </a>
-                                        <a href="{{ route('iot') }}" class="dropdown-link">
-                                            <span class="me-2">
-                                                <i class="flaticon-flash"></i>
-                                            </span>
-                                            <div class="drop-title">App Devlopment</div>
+                                        <a href="{{ route('mobileapp') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <i class="flaticon-layer"></i>
+                                                </span>
+                                            <div class="drop-title">Mobile App Development</div>
+                                        </a>
+                                        <a href="{{ route('product') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <i class="flaticon-vector"></i>
+                                                </span>
+                                            <div class="drop-title">Product Development</div>
                                         </a>
 
                                     </div>
                                     <div class="dropdown-grid-item bg-white">
                                         <h6 class="drop-heading">Learning and Placement</h6>
-                                        <a href="{{ route('iot') }}" class="dropdown-link">
-                                            <span class="me-2">
-                                                <i class="flaticon-clipboard"></i>
-                                            </span>
-                                            <div class="drop-title">Canada Placement</div>
+                                        <a href="{{ route('canada') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                        height="16" fill="currentColor" class="bi bi-coin"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
+                                                        <path
+                                                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                        <path
+                                                            d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+                                                    </svg>
+                                                </span>
+                                            <div class="drop-title">Canada Migration Program</div>
                                         </a>
-                                        <a href="{{ route('iot') }}" class="dropdown-link">
-                                            <span class="me-2">
-                                                <i class="flaticon-settings"></i>
-                                            </span>
-                                            <div class="drop-title">Us Placement</div>
+                                        <a href="{{ route('usa') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                        height="16" fill="currentColor" class="bi bi-bank"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.501.501 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89L8 0ZM3.777 3h8.447L8 1 3.777 3ZM2 6v7h1V6H2Zm2 0v7h2.5V6H4Zm3.5 0v7h1V6h-1Zm2 0v7H12V6H9.5ZM13 6v7h1V6h-1Zm2-1V4H1v1h14Zm-.39 9H1.39l-.25 1h13.72l-.25-1Z" />
+                                                    </svg>
+                                                </span>
+                                            <div class="drop-title">US Migration Program</div>
+                                        </a>
+                                        <a href="{{ route('staffing') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <i class="flaticon-avatar"></i>
+                                                </span>
+                                            <div class="drop-title">Staffing</div>
+                                        </a>
+                                        <a href="{{ route('placement') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                        height="16" fill="currentColor" class="bi bi-check2-circle"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
+                                                        <path
+                                                            d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
+                                                    </svg>
+                                                </span>
+                                            <div class="drop-title">Placement</div>
+                                        </a>
+                                        <a href="{{ route('recruitment') }}" class="dropdown-link">
+                                        <span class="me-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                        height="16" fill="currentColor"
+                                                        class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
+                                                    </svg>
+                                                </span>
+                                            <div class="drop-title">Recruitment</div>
                                         </a>
 
                                     </div>
